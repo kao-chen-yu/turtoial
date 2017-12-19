62d3f0c8dce9c76d9031303f03b407491e5d8c43
kao-chen-yu <ae6013@gmail.com>
2017-12-19 17:17:03 +0800

    add skill

--- src/main/java/com/example/myapp/DemoApplication.java
+++ src/main/java/com/example/myapp/DemoApplication.java
@@ -5,17 +5,57 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.da.coin.ide.spi.standard.ResultModel;
+import com.alibaba.da.coin.ide.spi.standard.TaskQuery;
+import com.alibaba.da.coin.ide.spi.standard.TaskResult;
+import com.alibaba.da.coin.ide.spi.trans.MetaFormat;
 
 @Controller
 @SpringBootApplication
 public class DemoApplication {
 	
-	@RequestMapping("/test")
+	@RequestMapping("/")
     @ResponseBody
     String home() {
       return "Hello World!";
+    }
+	
+	@RequestMapping(value = "/skill/weather", method = RequestMethod.POST)
+    public @ResponseBody ResultModel<TaskResult> getResponse(@RequestBody String taskQuery) {
+ 
+        /**
+         * 将开发者平台识别到的语义理解的结果（json字符串格式）转换成TaskQuery
+         */
+		System.out.println("---------skill ---------------");
+        TaskQuery query = MetaFormat.parseToQuery(taskQuery);
+ 
+        /**
+         * 构建服务返回结果
+         */
+        ResultModel<TaskResult> resultModel = new ResultModel<TaskResult>();
+        try {
+            /**
+             * 调用天气服务执行并构建回复内容
+             */
+            TaskResult result = new TaskResult();
+            result.setReply("------ testing ------");
+            resultModel.setReturnCode("0");
+            resultModel.setReturnValue(result);
+        } catch (Exception e) {
+            resultModel.setReturnCode("-1");
+            resultModel.setReturnErrorSolution(e.getMessage());
+        }
+ 
+        /**
+         * 直接返回ResultModel<TaskResult>对象就ok
+         */
+ 
+        return resultModel;
     }
 	
 	public static void main(String[] args) {
