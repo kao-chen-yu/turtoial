77af6170dec6d00c95e53693943e3910063dc2d2
kao-chen-yu <ae6013@gmail.com>
2017-12-19 16:59:58 +0800

    test

--- src/main/java/com/example/controller/IndexController.java
+++ src/main/java/com/example/controller/IndexController.java
@@ -1,16 +1,15 @@
 package com.example.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class IndexController {
 	
 	@RequestMapping("/")
-    @ResponseBody
-    String home() {
+	public String home() {
       return "Hello World!";
     }
 
--- src/main/java/com/example/myapp/DemoApplication.java
+++ src/main/java/com/example/myapp/DemoApplication.java
@@ -2,12 +2,15 @@ package com.example.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
+@Configuration
 @SpringBootApplication
+@ComponentScan()
 public class DemoApplication {
 	
 	public static void main(String[] args) {
