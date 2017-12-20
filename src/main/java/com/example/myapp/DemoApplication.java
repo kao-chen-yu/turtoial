871c1f2042bf91d9014ba48651ca31db4e739008
kao-chen-yu <ae6013@gmail.com>
2017-12-20 11:18:02 +0800

    ~~

--- src/main/java/com/example/myapp/DemoApplication.java
+++ src/main/java/com/example/myapp/DemoApplication.java
@@ -10,10 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.alibaba.da.coin.ide.spi.standard.ResultModel;
-import com.alibaba.da.coin.ide.spi.standard.TaskQuery;
-import com.alibaba.da.coin.ide.spi.standard.TaskResult;
-import com.alibaba.da.coin.ide.spi.trans.MetaFormat;
 
 @Controller
 @SpringBootApplication
@@ -26,36 +22,15 @@ public class DemoApplication {
     }
 	
 	@RequestMapping(value = "/skill/weather", method = RequestMethod.POST)
-    public @ResponseBody ResultModel<TaskResult> getResponse(@RequestBody String taskQuery) {
+    public @ResponseBody void getResponse(@RequestBody String taskQuery) {
  
         /**
          * 将开发者平台识别到的语义理解的结果（json字符串格式）转换成TaskQuery
          */
 		System.out.println("---------skill ---------------");
-        TaskQuery query = MetaFormat.parseToQuery(taskQuery);
+		System.out.println(taskQuery);
  
-        /**
-         * 构建服务返回结果
-         */
-        ResultModel<TaskResult> resultModel = new ResultModel<TaskResult>();
-        try {
-            /**
-             * 调用天气服务执行并构建回复内容
-             */
-            TaskResult result = new TaskResult();
-            result.setReply("------ testing ------");
-            resultModel.setReturnCode("0");
-            resultModel.setReturnValue(result);
-        } catch (Exception e) {
-            resultModel.setReturnCode("-1");
-            resultModel.setReturnErrorSolution(e.getMessage());
-        }
- 
-        /**
-         * 直接返回ResultModel<TaskResult>对象就ok
-         */
- 
-        return resultModel;
+        
     }
 	
 	public static void main(String[] args) {
